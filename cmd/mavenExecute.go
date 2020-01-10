17ef0a9e4d8ddbc59a038782798492c4726066d9
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-01-10 15:10:38 +0100

    update

--- cmd/mavenExecute.go
+++ cmd/mavenExecute.go
@@ -1,16 +1,46 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/SAP/jenkins-library/pkg/command"
 )
 
-func mavenExecute(MavenExecuteOptions mavenExecuteOptions) error {
+func mavenExecute(myMavenExecuteOptions mavenExecuteOptions) error {
+	parameters := []string{}
+
+	if myMavenExecuteOptions.GlobalSettingsFile != "" {
+		globalSettingsFileParameter := " --global-settings " + myMavenExecuteOptions.GlobalSettingsFile
+		parameters = append(parameters, globalSettingsFileParameter)
+	}
+
+	if myMavenExecuteOptions.ProjectSettingsFile != "" {
+		projectSettingsFileParameter := "--settings " + myMavenExecuteOptions.ProjectSettingsFile
+		parameters = append(parameters, projectSettingsFileParameter)
+	}
+
+	if myMavenExecuteOptions.M2Path != "" {
+		m2PathParameter := "-Dmaven.repo.local=" + myMavenExecuteOptions.M2Path
+		parameters = append(parameters, m2PathParameter)
+	}
+	if myMavenExecuteOptions.PomPath != "" {
+		pomPathParameter := "--file " + myMavenExecuteOptions.PomPath
+		parameters = append(parameters, pomPathParameter)
+	}
+
+	if myMavenExecuteOptions.Flags != nil {
+		parameters = append(parameters, myMavenExecuteOptions.Flags...)
+	}
+
+	parameters = append(parameters, "--batch-mode")
+
+	if myMavenExecuteOptions.LogSuccessfulMavenTransfers {
+		parameters = append(parameters, "-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn")
+	}
+
+	parameters = append(parameters, myMavenExecuteOptions.Goals...)
 
 	c := command.Command{}
-	c.RunShell("bash", "mvn clean package")
-	_, err := fmt.Printf("mvn")
+	c.RunExecutable("mvn", parameters...)
 
-	return err
+
+	return nil
 }
--- cmd/mavenExecute_generated.go
+++ cmd/mavenExecute_generated.go
@@ -1,24 +1,34 @@
 package cmd
 
 import (
+	"os"
+
 	"github.com/SAP/jenkins-library/pkg/config"
 	"github.com/SAP/jenkins-library/pkg/log"
 	"github.com/spf13/cobra"
 )
 
 type mavenExecuteOptions struct {
+	PomPath                     string   `json:"pomPath,omitempty"`
+	ProjectSettingsFile         string   `json:"projectSettingsFile,omitempty"`
+	GlobalSettingsFile          string   `json:"globalSettingsFile,omitempty"`
+	M2Path                      string   `json:"m2Path,omitempty"`
+	Goals                       []string `json:"goals,omitempty"`
+	Defines                     []string `json:"defines,omitempty"`
+	Flags                       []string `json:"flags,omitempty"`
+	LogSuccessfulMavenTransfers bool     `json:"logSuccessfulMavenTransfers,omitempty"`
 }
 
 var myMavenExecuteOptions mavenExecuteOptions
 var mavenExecuteStepConfigJSON string
 
-// MavenExecuteCommand Maven
+// MavenExecuteCommand Ths step allows to run maven commands
 func MavenExecuteCommand() *cobra.Command {
 	metadata := mavenExecuteMetadata()
 	var createMavenExecuteCmd = &cobra.Command{
 		Use:   "mavenExecute",
-		Short: "Maven",
-		Long:  `Maven`,
+		Short: "Ths step allows to run maven commands",
+		Long:  `Ths step allows to run maven commands`,
 		PreRunE: func(cmd *cobra.Command, args []string) error {
 			log.SetStepName("mavenExecute")
 			log.SetVerbose(GeneralConfig.Verbose)
@@ -34,7 +44,16 @@ func MavenExecuteCommand() *cobra.Command {
 }
 
 func addMavenExecuteFlags(cmd *cobra.Command) {
+	cmd.Flags().StringVar(&myMavenExecuteOptions.PomPath, "pomPath", os.Getenv("PIPER_pomPath"), "Path to the pom file that should be used.")
+	cmd.Flags().StringVar(&myMavenExecuteOptions.ProjectSettingsFile, "projectSettingsFile", os.Getenv("PIPER_projectSettingsFile"), "Path to the mvn settings file that should be used as project settings file.")
+	cmd.Flags().StringVar(&myMavenExecuteOptions.GlobalSettingsFile, "globalSettingsFile", os.Getenv("PIPER_globalSettingsFile"), "Path to the mvn settings file that should be used as global settings file.")
+	cmd.Flags().StringVar(&myMavenExecuteOptions.M2Path, "m2Path", os.Getenv("PIPER_m2Path"), "Path to the location of the local repository that should be used.*/")
+	cmd.Flags().StringSliceVar(&myMavenExecuteOptions.Goals, "goals", []string{}, "Maven goals that should be executed.")
+	cmd.Flags().StringSliceVar(&myMavenExecuteOptions.Defines, "defines", []string{}, "Additional properties in form of -Dkey=value.")
+	cmd.Flags().StringSliceVar(&myMavenExecuteOptions.Flags, "flags", []string{}, "Flags to provide when running mvn.")
+	cmd.Flags().BoolVar(&myMavenExecuteOptions.LogSuccessfulMavenTransfers, "logSuccessfulMavenTransfers", false, "Configures maven to log successful downloads. This is set to `false` by default to reduce the noise in build logs.")
 
+	cmd.MarkFlagRequired("goals")
 }
 
 // retrieve step metadata
@@ -42,7 +61,64 @@ func mavenExecuteMetadata() config.StepData {
 	var theMetaData = config.StepData{
 		Spec: config.StepSpec{
 			Inputs: config.StepInputs{
-				Parameters: []config.StepParameters{},
+				Parameters: []config.StepParameters{
+					{
+						Name:      "pomPath",
+						Scope:     []string{"PARAMETERS", "STEPS"},
+						Type:      "string",
+						Mandatory: false,
+						Aliases:   []config.Alias{},
+					},
+					{
+						Name:      "projectSettingsFile",
+						Scope:     []string{"PARAMETERS", "STEPS"},
+						Type:      "string",
+						Mandatory: false,
+						Aliases:   []config.Alias{},
+					},
+					{
+						Name:      "globalSettingsFile",
+						Scope:     []string{"PARAMETERS", "STEPS"},
+						Type:      "string",
+						Mandatory: false,
+						Aliases:   []config.Alias{},
+					},
+					{
+						Name:      "m2Path",
+						Scope:     []string{"PARAMETERS", "STEPS"},
+						Type:      "string",
+						Mandatory: false,
+						Aliases:   []config.Alias{},
+					},
+					{
+						Name:      "goals",
+						Scope:     []string{"PARAMETERS"},
+						Type:      "[]string",
+						Mandatory: true,
+						Aliases:   []config.Alias{},
+					},
+					{
+						Name:      "defines",
+						Scope:     []string{"PARAMETERS"},
+						Type:      "[]string",
+						Mandatory: false,
+						Aliases:   []config.Alias{},
+					},
+					{
+						Name:      "flags",
+						Scope:     []string{"PARAMETERS", "STEPS"},
+						Type:      "[]string",
+						Mandatory: false,
+						Aliases:   []config.Alias{},
+					},
+					{
+						Name:      "logSuccessfulMavenTransfers",
+						Scope:     []string{"PARAMETERS"},
+						Type:      "bool",
+						Mandatory: false,
+						Aliases:   []config.Alias{},
+					},
+				},
 			},
 		},
 	}

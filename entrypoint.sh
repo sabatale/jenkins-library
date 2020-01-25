48393daea169fd062076e49bcc2d36b0364f8d0c
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-01-25 17:15:47 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,4 @@
 #!/bin/sh -l
 set -x
 
-id
-
-
-ls -la /bin/piper
-
-find . -name pipeline_config.yml
-find . -name pom.xml
-
-/bin/piper mavenExecute --defines -Dmaven.test.skip=true --goals package
+/bin/piper version

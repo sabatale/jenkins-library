b6806cbffbc4edb70fb173bef959d5ea0fe0b9f3
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-02-06 22:05:36 +0100

    update

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 "use strict";
 
-// source: https://blog.myitcv.io/2020/02/04/portable-ci-cd-with-pure-go-github-actions.html
-
-const { spawnSync} = require('child_process');
-
+const { spawn } = require('child_process');
 
 (async function() {
-  const a = spawnSync("go", ["build", "-o", "piper", "."], {cwd: __dirname})
-console.log('stdout ', a.stdout.toString());
-console.log('stderr ', a.stderr.toString());
-  const b = spawnSync(__dirname + "/piper", ["mavenExecute", "--goals", "verify"], {cwd: "/home/runner/work/piper-go-actions-playground/piper-go-actions-playground"})
-console.log('stdout ', b.stdout.toString());
-console.log('stderr ', b.stderr.toString());
+    spawn("go", ["build", "-o", "piper", "."], {cwd: __dirname, stdio: 'inherit'})
+    spawn(__dirname + "/piper", ["mavenExecute", "--goals", "verify"], {stdio: 'inherit', cwd: "/home/runner/work/piper-go-actions-playground/piper-go-actions-playground"})
 })();

366b7277ec46a973740d7983c1abea9e56418cf0
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-02-06 22:12:05 +0100

    await

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 const { spawn } = require('child_process');
 
 (async function() {
-    spawn("go", ["build", "-o", "piper", "."], {cwd: __dirname, stdio: 'inherit'})
-    spawn(__dirname + "/piper", ["mavenExecute", "--goals", "verify"], {stdio: 'inherit', cwd: "/home/runner/work/piper-go-actions-playground/piper-go-actions-playground"})
+    await spawn("go", ["build", "-o", "piper", "."], { cwd: __dirname, stdio: 'inherit' })
+    await spawn(__dirname + "/piper", ["mavenExecute", "--goals", "verify"], { stdio: 'inherit', cwd: "/home/runner/work/piper-go-actions-playground/piper-go-actions-playground" })
 })();
